2700b8d23ea1fe7dd208ba39ec62441f5811a585
Christiantyemele <yemelechristian2@gmail.com>
2024-04-25 09:10:57 +0100

    adding score display

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import './style.css'
-import GameManager from "./Game";
+import ScoreBlue from './Scores'
 import reportWebVitals from './reportWebVitals';
 import Board from "./Board";
+import './index.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Board />
+    <React.StrictMode>
 
-  </React.StrictMode>
+      <ScoreBlue/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
