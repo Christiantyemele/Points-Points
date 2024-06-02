53dfa9a461ec038b94260363c2a03b5c16aa26af
Christiantyemele <yemelechristian2@gmail.com>
2024-06-02 12:39:05 +0100

    fix(scores): fixing score alignment

--- src/Scores.tsx
+++ src/Scores.tsx
@@ -1,14 +1,17 @@
 import Board from "./Board";
 import './index.css'
-import {score} from "./Board";
+import {SCORES} from "./App";
+import {useContext} from "react";
 export default function  Score() {
+    // const {A, a} = useContext(SCORES);
     return (
         <div className="main-container">
                 <div className="scores">
-                    <div className="red"><button className=" btn-xs sm:btn-sm bg-blue-600">{score.score_red}</button></div>
-                    <div className="blue"><button className=" btn-xs sm:btn-sm bg-red-600">{score.score_blue}</button>
-                    </div>
+                    <div className="red"><button className=" btn-xs sm:btn-sm bg-blue-600">{0}</button>
+                    <div className="blue"><button className=" btn-xs sm:btn-sm bg-red-600">{0}</button>
                 </div>
+                    </div>
+                    </div>
             <Board/>
         </div>
 
